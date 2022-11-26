e5f268a71a14a6ccb5a475d208c400befc2ab2ef
devruhul <ruhulamin262001@gmail.com>
2022-11-27 01:43:11 +0600

    feat: adde antd navigation with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import Blogs from "./pages/Blogs/Blogs";
+import Products from "./Products/Products";
+import Footer from "./Shared/FooterMenu/FooterMenu";
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
 
+const items = [
+  { label: "Home", key: "/" },
+  { label: "About", key: "/about" },
+  { label: "Contact", key: "/contact" },
+  { label: "Login", key: "/login" },
+  { label: "Register", key: "/register" },
+  { label: "Logout", key: "/logout" },
+];
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header>
+        <div className='logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          onClick={(e) => {
+            navigate(e.key);
+          }}
+          defaultSelectedKeys={["/"]}
+          items={items}
+        />
+      </Header>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Shared/FooterMenu/FooterMenu.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "antd";
+
+const { Footer } = Layout;
+const FooterMenu = () => {
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      Walton E-Plaza ©2022 Created by Ruhul Amin
+    </Footer>
+  );
+};
+
+export default FooterMenu;
